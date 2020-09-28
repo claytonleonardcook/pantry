c866959d221519c55ca60e16915d64a286e36aeb
Clayton Cook <claytonleonardcook@gmail.com>
2020-09-28 18:29:13 -0400

    Added login button events

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
+  const history = useHistory();
   return (
     <div className="Login">
       <h1>PANTRY</h1>
       <form>
         <div>
           <label>Email</label>
-          <input type='text' />
+          <input id='email' type='text' />
         </div>
         <div>
           <label>Password</label>
-          <input type='password' />
+          <input id='password' type='password' />
         </div>
         <hr />
-        <button>Sign In</button>
-        <button>Create Account</button>
+        <button onClick={() => {
+          history.push('/home');
+        }}>Sign In</button>
+        <button onClick={() => {
+          console.log(document.querySelector('input#email').value,
+            document.querySelector('input#password').value)
+        }}>Create Account</button>
       </form>
       <span>PANTRY | Developed by @claytoncook</span>
     </div>
