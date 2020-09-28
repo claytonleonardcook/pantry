5b85dcea09d5662313a50f5b951937f1ce6615a2
Clayton Cook <claytonleonardcook@gmail.com>
2020-09-28 18:38:17 -0400

    Build

---
+++ build/precache-manifest.044f22f85a39625b7f5a334a8cbdf884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3da22e6b7eadd3fd47aa5fc5a50a6c7",
+    "url": "/Grocery-List-App/build/index.html"
+  },
+  {
+    "revision": "7f11c9df33477b9e74c5",
+    "url": "/Grocery-List-App/build/static/css/main.a3b46f74.chunk.css"
+  },
+  {
+    "revision": "4b981edeceb603c7512b",
+    "url": "/Grocery-List-App/build/static/js/2.7b520b76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Grocery-List-App/build/static/js/2.7b520b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f11c9df33477b9e74c5",
+    "url": "/Grocery-List-App/build/static/js/main.ad36150d.chunk.js"
+  },
+  {
+    "revision": "e532bf57458c69ffd88f",
+    "url": "/Grocery-List-App/build/static/js/runtime-main.ffe34060.js"
+  }
+]);
