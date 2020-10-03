e8676f12e9a23788f425c9e7df50376f53fa8e19
Clayton Cook <claytonleonardcook@gmail.com>
2020-10-02 21:55:58 -0400

    Worked on filtering functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Item.scss';
 
 function Item(props) {
+    console.log(props.value)
     return (
         <li className="Item">
             <input type="checkbox" />
-            {props.value}
+            {props.value[0]}
         </li>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,53 @@ import './Home.scss';
 import List from '../components/List';
 
 function Home() {
-  let [list, setList] = useState(['Milk', 'Vegatables', 'Fruit', 'Eggs']);
+  let [list, setList] = useState(
+    {
+      Milk: true,
+      Eggs: false,
+      Vegtables: false,
+      Meat: true
+    }
+  );
   let [sortedList, setSortedList] = useState([]);
+
   return (
     <div className="Home">
       <div>
         <div>
           <input id='addItem' placeholder='Add Item' />
           <button onClick={() => {
-            setList([...list, document.querySelector('input#addItem').value]);
+            let newList = list;
+            newList[document.querySelector('input#addItem').value] = false
+            setList(newList);
             document.querySelector('input#addItem').value = null;
           }}>+</button>
         </div>
         <div>
-          <button onClick={() => {
-            setSortedList(list);
-          }}>All</button>
-          <button onClick={() => {
-            //setSortedList(list.filter());
-          }}>Checked</button>
-          <button>Unchecked</button>
+          <form>
+            <div>
+              <input type='radio' name='filter' value='All' defaultChecked onChange={() => {
+                setSortedList(Object.entries(list));
+              }} />
+              <label>All</label>
+            </div>
+            <div>
+              <input type='radio' name='filter' value='Checked' onChange={() => {
+                setSortedList(Object.entries(list).filter((item) => {
+                  return item[1] ? [item[0], item[1]] : null;
+                }));
+              }} />
+              <label>Checked</label>
+            </div>
+            <div>
+              <input type='radio' name='filter' value='Unchecked' onChange={() => {
+                setSortedList(Object.entries(list).filter((item) => {
+                  return !item[1] ? [item[0], item[1]] : null;
+                }));
+              }}/>
+              <label>Unchecked</label>
+            </div>
+          </form>
         </div>
       </div>
       <hr />
