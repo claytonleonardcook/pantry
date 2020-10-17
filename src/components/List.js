2a4ba1968605baf424acb018614451b055a69ca9
Clayton Cook <claytonleonardcook@gmail.com>
2020-10-17 19:07:21 -0400

    Added style changes and animations

--- src/components/List.js
+++ src/components/List.js
@@ -13,10 +13,10 @@ function List({ list, filters, user }) {
                     const { checked, unchecked } = filters;
                     const item = list[key];
                     return ((checked && unchecked) || (checked && item.checked) || (unchecked && !item.checked));
-                }).map((key) => {
+                }).map((key, index) => {
                     const { name, checked } = list[key];
                     return (
-                        <li key={key}>
+                        <li key={key} style={{animationDuration: `${((index/8)+1.5)}s`, textDecoration: 'line-through'}}>
                             <input type="checkbox" value={checked} checked={checked} onChange={() => {
                                 firebase.database().ref(`${user.uid}/${key}/checked`).set(!checked);
                             }} />
